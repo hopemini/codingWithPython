3d83141900bf55149bb9b5d3b595fc4871e7d997
Sungmin Choi <hopemini@gmail.com>
2022-05-16 23:23:25 +0900

    Update BFS

---
+++ mooc/c4.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+def Input_Data():
+    readl = sys.stdin.readline
+    N, M = map(int, readl().split())
+    R, C, S, K = map(int, readl().split())
+    return N, M, R, C, S, K
+
+
+sol = -1
+N, M, R, C, S, K = Input_Data()
+
+#N, M, R, C, S, K = 9, 9, 3, 5, 2, 8
+
+def bfs():
+    q = deque()
+    d = ((-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2))
+    chk = [ [0] * (M+1) for _ in range(N+1)]
+    q.append((R, C, 0))
+    chk[3][5] = 1
+    while q:
+        h, w, t = q.popleft()
+        for dh, dw in d:
+            nh, nw, nt = h + dh, w + dw, t + 1
+            if not 1 <= nh <= N: continue
+            if not 1 <= nw <= M: continue
+            if chk[nh][nw] == 1: continue
+
+            if nh == S and nw == K:
+                return nt
+            q.append((nh, nw, nt))
+            chk[nh][nw] = 1
+    return -1
+
+sol = bfs()
+print(sol)
---
+++ mooc/c5.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+def Input_Data():
+    readl = sys.stdin.readline
+    M, N = map(int, readl().split())
+    map_box = [[0] + list(map(int, readl().split())) + [0] if 1<=r<=N else [0] * (M+2) for r in range(N+2)]
+    return M, N, map_box
+
+
+sol = -1
+M, N, map_box = Input_Data()
+
+def bfs():
+    q = deque()
+    d = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    for i in range(N+2):
+        for j in range(M+2):
+            if map_box[i][j] == 1:
+                q.append((i, j, 0))
+    while q:
+        h, w, t = q.popleft()
+        for dh, dw in d:
+            nh, nw, nt = h + dh, w + dw, t + 1
+            if not 1 <= nh <= N: continue
+            if not 1 <= nw <= M: continue
+            if map_box[nh][nw] == 1 or map_box[nh][nw] == -1: continue
+
+            q.append((nh, nw, nt))
+            map_box[nh][nw] = 1
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            if map_box[i][j] == 0:
+                return -1
+    return t
+
+sol = bfs()
+print(sol)
