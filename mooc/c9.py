3eef8b20dedb1148cd74ffc688739e620e903b31
Sungmin Choi <sungmin.choi@lge.com>
2022-05-25 10:46:58 +0900

    Update mooc

---
+++ mooc/c9.py
@@ -0,0 +1,58 @@
+import sys
+ 
+def Input_Data():
+    readl = sys.stdin.readline
+    N, M = map(int, readl().split())
+    return N, M
+ 
+ 
+def Dice1(n):
+    # n번째 주사위 던졌을때 나올 숫자 선택 (중복순열)
+    if n >= N:
+        print(*dice)
+        return
+ 
+    for i in range(1, 6+1):
+        # n번째 주사위 숫자 -> i
+        dice[n] = i
+        Dice1(n+1)
+ 
+ 
+def Dice2(n, s):
+    # 중복 조합
+    if n>=N:
+        print(*dice)
+        return
+ 
+    for i in range(s,6+1):
+        dice[n] = i
+        Dice2(n+1, i)
+ 
+ 
+def Dice3(n):
+    # 순열
+    if n >= N:
+        print(*dice)
+        return
+ 
+    for i in range(1,6+1):
+        if sel[i]: continue
+        dice[n] = i
+        sel[i] = 1
+        Dice3(n+1)
+        sel[i] = 0
+ 
+ 
+def Solve():
+    # 주사위 던지기 순서번호 : 0 ~ N-1
+    if M == 1: Dice1(0) # n : 주사위 던지기 순서번호
+    elif M == 2: Dice2(0, 1) # n : 주사위 던지기 순서번호, s : 선택 시작 숫자
+    elif M == 3: Dice3(0) # n : 주사위 던지기 순서번호
+ 
+# 입력 받는 부분
+N, M = Input_Data()
+ 
+# 여기서부터 작성
+dice = [0] * N  # dice[n] : n번째 던지 주사위의 숫자
+sel = [0] * (7) # sel[n] : n숫자의 현재 진행중인 경우의 수에서 선택 여부 (순열 경우의 수 시도에서 사용)
+Solve()
---
+++ mooc/d1.py
@@ -0,0 +1,39 @@
+import sys
+
+def Input_Data():
+    readl = sys.stdin.readline
+    N = int(readl())
+    matrix = [[0] + list(map(int, readl().split())) if 1<=n<=N else [0] * (N+1)  for n in range(N+1)]
+    return N, matrix
+
+
+sol = -1
+# 입력 받는 부분
+N, matrix = Input_Data()
+
+# 여기서부터 작성
+def dfs(n, sum):
+    global sol
+    if sol <= sum:
+        return
+    if n > N:
+        sol = sum
+        return
+    for i in range(1, N+1):
+        if chk[i]:
+            continue
+        chk[i] = True
+        dfs(n+1, sum + matrix[n][i])
+        chk[i] = False
+
+def Solve():
+    global sol
+    sol = 100 * N
+    dfs(1, 0)
+    return sol
+
+chk = [False] * (N+1)
+sol = Solve()
+
+# 출력하는 부분
+print(sol)
